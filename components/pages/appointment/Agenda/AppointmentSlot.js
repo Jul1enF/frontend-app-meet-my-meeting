7e08b530ca0d2dff4411fc038fb08d044cb1094b
Jul1enF <j.furic@gmail.com>
2026-06-20 13:50:48 +0200

    sortFreeEmployees is a string now

--- components/pages/appointment/Agenda/AppointmentSlot.js
+++ components/pages/appointment/Agenda/AppointmentSlot.js
@@ -12,17 +12,17 @@ export default memo(function AppointmentSlot({ start, employees, setSelectedAppo
     const sortAvailableEmployees = (a, b) => {
         let roleDifference
 
-        if (sortFreeEmployees.role) roleDifference = rolesPriorities[a.role] - rolesPriorities[b.role]
+        if (sortFreeEmployees === "role") roleDifference = rolesPriorities[a.role] - rolesPriorities[b.role]
         if (roleDifference && roleDifference !== 0) return roleDifference
 
         let msOfWorkDifference
 
-        if (sortFreeEmployees.msOfWork) msOfWorkDifference = b.msOfWork - a.msOfWork
+        if (sortFreeEmployees === "msOfWork") msOfWorkDifference = b.msOfWork - a.msOfWork
         if (msOfWorkDifference && msOfWorkDifference !== 0) return msOfWorkDifference
 
         let eventCountDifference
 
-        if (sortFreeEmployees.eventCount) eventCountDifference = b.eventCount - a.eventCount
+        if (sortFreeEmployees === "eventCount") eventCountDifference = b.eventCount - a.eventCount
         if (eventCountDifference && eventCountDifference !== 0) return eventCountDifference
 
         return new Date(a.createdAt) - new Date(b.createdAt)
